585f6b4dc1bd63b2441ff043f9e9fe25fa16e3eb
HACP <hugo.a.contreras.p@gmail.com>
2022-10-08 23:00:10 -0400

    Added tests for entropy and MI
    Numerical tests with entropy and MI

--- code/src/InformationTheoryMetricsLib.py
+++ code/src/InformationTheoryMetricsLib.py
@@ -59,14 +59,88 @@ def get_entropy(x,p=2,k=1):
     tree = KDTree(x)
     all_distances, indices = tree.query(x, k + 1, p=p)
     distances_to_k_neighbor = all_distances[:,-1]
-    print(all_distances)
 
     sum_log_distances_to_k_neighbor = np.sum(np.log(2*distances_to_k_neighbor))
     h = -digamma(k) + digamma(n) + log_c_d_p + (1.*d/n) * sum_log_distances_to_k_neighbor
 
     return(h)
 
+def get_mutual_information(x,y,p=2,k=1,method='both'):
+    """
+    Computes the estimated mutual information for two continuous distributions following the
+    Kraskov, Stogbauer and Grassberger (KSG) method (PhysRevE.69.066138 - 2014)
+    Reference: https://journals.aps.org/pre/pdf/10.1103/PhysRevE.69.066138
+    Method 1: Balls Equation 23
+    Method 2: Rectangle Equation 30
+
+    Parameters:
+    x:  array-like
+        points in the distribution
+    y:  array-like
+        points in the distribution
+    p:  scalar (p>=1)
+        p value of the p-norm.
+            p = 1, Manhattan
+            p = 2, Eucledian
+            p = np.inf, Max
+    k:  scalar
+        k neighbor
+    method: 'balls','rectangles' or 'both'
+
+    Returns:
+    h:  scalar
+        Approximated entropy for the distribution
+    """
+
+    # NOTE: do x,y need to be the same size?
+
+    if len(x.shape) == 2:
+        nx, dx = x.shape
+    if len(y.shape) == 2:
+        ny, dy = y.shape
+    else:
+        raise Exception("Input data set should be a 2-dimensional array")
+
+    if p>=1:
+        log_c_d_p = get_log_volume_ball_d_p(1, dx, p)
+    else:
+        #raise Exception("Input p must be p>=1")
+        return('Nope')
+
+    xy = np.c_[x,y]
+
+    tree_x = KDTree(x)
+    tree_y = KDTree(y)
+    tree_xy = KDTree(xy)
+
+    all_distances, indices = tree_xy.query(xy, k + 1, p=p)
+    radii = all_distances[:,-1]
+
+    lnx = np.zeros(nx)
+    lny = np.zeros(ny)
+
+    for i_dim in range(nx):
+        lnx[i_dim] = len(tree_x.query_ball_point(tree_x.data[i_dim], r=radii[i_dim], p=p)) - 1
+        lny[i_dim] = len(tree_y.query_ball_point(tree_y.data[i_dim], r=radii[i_dim], p=p)) - 1
+
+    mi_balls = digamma(k) - np.mean(digamma(lnx+1) + digamma(lny+1)) + digamma(nx)
+    mi_rectangles = digamma(k) -1./k - np.mean(digamma(lnx) + digamma(lny)) + digamma(nx)
+
+    if method == 'balls':
+        return(mi_balls)
+    if method == 'rectangles':
+        return(mi_rectangles)
+    if method == 'both':
+        return(np.mean(mi_balls+mi_rectangles))
+    else:
+        #raise Exception("Method should be 'balls','rectangles' or 'both'")
+        return('Nope')
 
 
 
-#print(get_log_volume_ball_d_p(1,2,np.inf))
+
+x = np.random.randn(10,2)
+y = np.random.randn(10,2)
+
+print(get_mutual_information(x,y,p=2,k=1,method='balls'))
+print(get_mutual_information(x,y,p=2,k=1,method='rectangles'))
--- code/test/InformationTheoryMetricsLib_test.py
+++ code/test/InformationTheoryMetricsLib_test.py
@@ -2,7 +2,7 @@ from src.InformationTheoryMetricsLib import *
 import pytest
 
 def test_get_entropy_valid_p():
-    x = []
+    x = np.random.normal(size=(100,1))
     p = -1
     assert get_entropy(x,p) == 'Nope'
 
@@ -16,5 +16,17 @@ def test_get_log_volume_ball_d_p():
 
     logV1_1 = get_log_volume_ball_d_p(D, d1, p1) # volume of 1-dimensional ball, Manhattan. V1_1(D) = D
     logV2_2 = get_log_volume_ball_d_p(D, d2, p2) # volume of 2-dimensional ball, Eucledian. V2_2(D) = pi*D^2/4
-    logV3_inf = get_log_volume_ball_d_p(D, d3, p3) # volume of 3-dimensional ball, Max norm. V3_inf(D) = D^d3
+    logV3_inf = get_log_volume_ball_d_p(D, d3, p3) # volume of 3-dimensional ball, Max norm. V3_inf(D) = D^3
     assert np.linalg.norm(np.array([logV1_1, logV2_2, logV3_inf]) - np.array([0., np.log(np.pi/4),0])) < 0.01
+
+def test_get_entropy_gaussian():
+    # https://gregorygundersen.com/blog/2020/09/01/gaussian-entropy/
+    x = np.random.normal(size=(100000,1))
+    assert abs(0.5*np.log(2*np.pi) + 0.5 - get_entropy(x,p=2,k=5))<0.001
+
+def test_get_mutual_information_multivariate_gaussian():
+    mean = [0, 0]
+    cov = [[1, 0], [0, 100]]
+    NN = 50000
+    x, y = np.random.multivariate_normal(mean, cov, NN).T
+    assert abs(0 - get_mutual_information(x.reshape(NN,1),y.reshape(NN,1),p=np.inf,k=10,method='balls')) < 0.01
