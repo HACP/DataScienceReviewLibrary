98a0a6898fc632c25902885bc52f11a4c53abb6d
HACP <hugo.a.contreras.p@gmail.com>
2022-10-12 22:52:18 -0400

    Added clustering based on free energy
    very slow, global max is not guaranteed. Good start, a lots to do

--- code/src/InformationTheoryClusteringLib.py
+++ code/src/InformationTheoryClusteringLib.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import pylab as plt
 from scipy.spatial.distance import cdist, pdist, squareform
+from scipy.spatial import KDTree
 from InformationTheoryMetricsLib import get_normalized_mutual_information_mixed as NMI
 
 def get_kMeans(df, k, verbose=0):
@@ -56,6 +57,89 @@ def get_kMeans(df, k, verbose=0):
     df['label'] = df_km['closest_centroid']
     return(df)
 
+def get_max_free_energy_clustering(df, N_C, T = 1000, k = 10, N_iterations = 1000):
+    """
+    Computes max free energy clustering for an arbitrary (n,d) data set.
+    The algorithm maximized the "free energy" associated to the entropy and the similarity matrix.
+    F = mean_s - T * I
+    where mean_s is the mean value of the similarity measure, averaged over the probability P_I_C
+    and I is the information content.
+    We created a greedy way to find the minimum - too slow. This version leverages local probability
+    Reference: https://www.princeton.edu/~wbialek/our_papers/slonim+al_05b.pdf
+
+    Note: the algorithm can find local maxima - e.g. a true 3-cluster mapped with 2 computed clusters.
+    Need to think a way to discourage high intra cluster distances/
+
+    Parameters:
+    df: data frame (n,d) dimension
+        n points in a R^d space
+
+    N_C:  scalar
+        number of clusters
+
+    T:  scalar
+        Lagrange multiplier -
+
+    k:  integer
+        Number of k nearest neighbors to be flipped to mean probability
+
+    N_iterations:   scalar
+        Maximum number of iterations
+
+    Returns:
+    df: data frame (n,d+1) dimension
+        same input data frame with an additional column called 'label' with
+        the cluster index
+    """
+
+    N = len(df)
+    def initialize_P_I_C(N):
+        # initializing the array containing the probability that point I belongs to cluster C
+        nn_P_I_C = np.random.rand(N,N_C)
+        P_I_C = nn_P_I_C/np.sum(nn_P_I_C,axis=1).reshape(-1,1)
+        return(P_I_C)
+
+    tree = KDTree(df) # kd tree to evaluate k nearest neighbors
+
+    # defining similarity metric based on the distance TO DO: include p as an option
+    s = squareform(pdist(df))
+    s = np.exp(-s)
+
+    P_I_C = initialize_P_I_C(N)
+
+    # starting the loop to maximze F
+    Fmax = -10000000
+
+    for _ in range(N_iterations):
+        # selecting one point at random, and finding coordinates
+        rand_sample_index = np.random.randint(N, size=1)
+        df_sample = df.iloc[rand_sample_index]
+        # finding k nearest neighbors
+        dist, index_list = tree.query(df_sample,k)
+        # defining working copy of P_I_C and assign mean value of P_I_C to the neighborhood
+        P_I_C_test = P_I_C.copy()
+        P_I_C_test[index_list] = np.mean(P_I_C_test[index_list],axis=1)
+
+        # compute the average similarity among elements chosen independently out of a single cluster
+        s_C = np.diag(np.matmul(np.matmul(np.transpose(P_I_C_test),s),P_I_C_test))
+        # total probability of finding any element in cluster C
+        P_C = np.sum(P_I_C_test,axis=0)*(1/N)
+        # compute mean similarity across all data points and clusters
+        mean_s = np.dot(s_C,P_C)
+        # compute information carried by clusters
+        I = np.sum(np.log(P_I_C_test*1/P_C)*P_I_C_test)/N
+        # define free energy
+        F = mean_s - T*I
+
+        if F > Fmax:
+        #print("Minimization",F)
+            Fmax = F
+            P_I_C = P_I_C_test
+
+    df['label'] = np.argmax(P_I_C,axis=1)
+    return(df)
+
+
 mean1 = [0, 0]
 cov1 = [[1, 0], [0, 1]]
 NN = 500
@@ -77,6 +161,7 @@ df = pd.concat([df1,df2,df3])
 
 print(df.head())
 print(get_kMeans(df, k=3, verbose = 1))
+print(get_max_free_energy_clustering(df, N_C=3))
 
 df1_truth = pd.DataFrame(np.transpose([x1,y1]))
 df1_truth['true_label'] = 0
